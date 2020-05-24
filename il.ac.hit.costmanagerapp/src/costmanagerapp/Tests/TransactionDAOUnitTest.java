fb52b0b410d417212d36fb77eecddf322b2efa05
Nir Chodorov <nirhodorov@gmail.com>
2020-05-24 22:09:20 +0300

    Done with DAO - without config files and without hybernet

--- il.ac.hit.costmanagerapp/src/costmanagerapp/Tests/TransactionDAOUnitTest.java
+++ il.ac.hit.costmanagerapp/src/costmanagerapp/Tests/TransactionDAOUnitTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.Collection;
 
 public class TransactionDAOUnitTest {
 
@@ -40,12 +41,56 @@ public class TransactionDAOUnitTest {
         System.out.println(cl.getGuid() + ", "+ cl.getPrice() +", " + cl.getDateOfTransaction());
     }
 
+
+    @Test
+    public void testGetTransactionByUser() throws UsersPlatformException {
+        Collection<Transaction> cl = tester.getTransactionByUser(1);
+        if(cl.size() == 0) throw new AssertionError("empty list");
+        cl.forEach(c -> System.out.println(c.getGuid() +", "+ c.getPrice()));
+    }
+
+    @Test
+    public void testUpdateTransaction() throws ClassNotFoundException  {
+        LocalDate d = LocalDate.of(2020, 05, 10);
+        String desc = "Lord Of The Rings";
+        IRetailDAO retailDAO = new MySqlRetailDAO();
+        IUsersDAO usersDAO = new MySqlUserDAO();
+        try {
+            tester.updateTransaction(1234, true, 21, desc, 52790, d);
+        } catch (UsersPlatformException e) {
+            e.printStackTrace();
+            throw new AssertionError();
+        }
+    }
+
+    @Test
+    public void testGetTransactionByRetail() throws UsersPlatformException {
+        Collection<Transaction> cl = tester.getTransactionByRetail(52790);
+        if(cl.size() == 0) throw new AssertionError("empty list");
+        cl.forEach(c -> System.out.println(c.getGuid() +", "+ c.getRetail()));
+    }
+
+    @Test
+    public void testGetTransactionByDateRange() throws UsersPlatformException {
+        LocalDate d1 = LocalDate.of(2020, 05, 10);
+        LocalDate d2 = LocalDate.of(2020, 05, 15);
+        Collection<Transaction> cl = tester.getTransactionByDateRange(d1, d2);
+        if(cl.size() == 0) throw new AssertionError("empty list");
+        cl.forEach(c -> System.out.println(c.getGuid() +", "+ c.getDateOfTransaction()));
+    }
+
+    @Test
+    public void testGetTransactionByPriceRange() throws UsersPlatformException {
+        Collection<Transaction> cl = tester.getTransactionByPriceRange(0, 21);
+        if(cl.size() == 0) throw new AssertionError("empty list");
+        cl.forEach(c -> System.out.println(c.getGuid() +", "+ c.getPrice()));
+    }
     @Test
     public void testInsertTransaction() throws ClassNotFoundException {
         IRetailDAO retailDAO = new MySqlRetailDAO();
         IUsersDAO usersDAO = new MySqlUserDAO();
         try {
-            tester.insertTransaction(new Transaction(11, false, 54.522, retailDAO.getRetails().stream().findFirst().get(),
+            tester.insertTransaction(new Transaction(19, false, 54.522, retailDAO.getRetails().stream().findFirst().get(),
                     usersDAO.getUser(1), LocalDate.now(), "Test etset"));
         } catch (SQLException e) {
             e.printStackTrace();
--- il.ac.hit.costmanagerapp/src/costmanagerapp/lib/DAO/ITransactionDAO.java
+++ il.ac.hit.costmanagerapp/src/costmanagerapp/lib/DAO/ITransactionDAO.java
@@ -1,5 +1,6 @@
 package costmanagerapp.lib.DAO;
 
+import com.sun.istack.internal.NotNull;
 import costmanagerapp.lib.Models.Transaction;
 import costmanagerapp.lib.UsersPlatformException;
 
@@ -12,10 +13,16 @@ public interface ITransactionDAO {
 
     Transaction getTransaction(int transaction_id) throws UsersPlatformException;
 
-    Collection<Transaction> getUserTransactions(int userId) throws UsersPlatformException;
+    Collection<Transaction> getTransactionByUser(int userId) throws UsersPlatformException;
 
-    void updateTransaction(int guid, boolean isIncome, float price, String description,
-                           String retailName, LocalDate sdf) throws SQLException;
+    Collection<Transaction> getTransactionByRetail(int retailId) throws UsersPlatformException;
+
+    Collection<Transaction> getTransactionByDateRange(LocalDate from, LocalDate to) throws UsersPlatformException;
+
+    Collection<Transaction> getTransactionByPriceRange(double fromPrice, double toPrice) throws UsersPlatformException;
+
+    void updateTransaction(@NotNull int guid, @NotNull boolean isIncome, @NotNull double price, @NotNull String description,
+                           @NotNull int retailGuid, @NotNull LocalDate dateOT) throws UsersPlatformException;
 
     void insertTransaction(Transaction transaction) throws SQLException, UsersPlatformException;
 
