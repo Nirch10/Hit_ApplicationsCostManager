1132b7134fd361fb02863d7582059e5704b63c4c
Sapir Chodorov <sapch8@gmail.com>
2020-08-04 23:20:19 +0300

    added client side

--- il.ac.hit.costmanagerapp/src/costmanagerapp/Console/RestMain.java
+++ il.ac.hit.costmanagerapp/src/costmanagerapp/Console/RestMain.java
@@ -13,9 +13,14 @@ import costmanagerapp.lib.UsersPlatformException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class RestMain {
     public static void main(String args[]) throws IOException, UsersPlatformException {
+        Path currentRelativePath = Paths.get("");
+        String s = currentRelativePath.toAbsolutePath().toString();
+        System.out.println("Current relative path is: " + s);
         CostManagerDAOConfig config = CostManagerDAOConfigWrapper.Deserialize("./Config.json");
         RestModelConnector restModelConnector =
                 new RestModelConnector(new HnetMySqlUserDAO(config), new HnetMySqlRetailsDAO(config),
