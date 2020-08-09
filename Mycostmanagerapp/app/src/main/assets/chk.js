718b86a84f8ca319e30a8c20799cb1e98047d5a1
Sapir Chodorov <sapch8@gmail.com>
2020-08-09 21:51:25 +0300

    DOne and dusted

--- Mycostmanagerapp/app/src/main/assets/chk.js
+++
@@ -1,7 +0,0 @@
-function calc()
-{
-    var a = parseInt(document.myform.num_a.value,10);
-    var b = parseInt(document.myform.num_b.value,10);
-    var sum = window.ob.calc_sum(a,b);
-    document.myform.result.value = sum;
-}
